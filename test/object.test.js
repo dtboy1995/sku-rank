7701cb7efa21cc5e58036ced5a653c0937a8f628
dtboy <936776640@qq.com>
2019-12-20 14:22:48 +0800

    add test case

---
+++ test/object.test.js
@@ -0,0 +1,30 @@
+const rank = require('../dist/rank').default
+
+test('object type', function () {
+    let data = [
+        [{ name: '白色' }, { name: '黑色' }, { name: '灰色' }],
+        [{ name: 'S' }, { name: 'M' }, { name: 'L' }],
+        [{ name: '真皮' }, { name: '尼龙' }]
+    ]
+    let ret = rank(data, 'item')
+    expect(ret).toEqual([
+        [{ name: '白色' }, { name: 'S' }, { name: '真皮' }],
+        [{ name: '白色' }, { name: 'S' }, { name: '尼龙' }],
+        [{ name: '白色' }, { name: 'M' }, { name: '真皮' }],
+        [{ name: '白色' }, { name: 'M' }, { name: '尼龙' }],
+        [{ name: '白色' }, { name: 'L' }, { name: '真皮' }],
+        [{ name: '白色' }, { name: 'L' }, { name: '尼龙' }],
+        [{ name: '黑色' }, { name: 'S' }, { name: '真皮' }],
+        [{ name: '黑色' }, { name: 'S' }, { name: '尼龙' }],
+        [{ name: '黑色' }, { name: 'M' }, { name: '真皮' }],
+        [{ name: '黑色' }, { name: 'M' }, { name: '尼龙' }],
+        [{ name: '黑色' }, { name: 'L' }, { name: '真皮' }],
+        [{ name: '黑色' }, { name: 'L' }, { name: '尼龙' }],
+        [{ name: '灰色' }, { name: 'S' }, { name: '真皮' }],
+        [{ name: '灰色' }, { name: 'S' }, { name: '尼龙' }],
+        [{ name: '灰色' }, { name: 'M' }, { name: '真皮' }],
+        [{ name: '灰色' }, { name: 'M' }, { name: '尼龙' }],
+        [{ name: '灰色' }, { name: 'L' }, { name: '真皮' }],
+        [{ name: '灰色' }, { name: 'L' }, { name: '尼龙' }]
+    ])
+})
